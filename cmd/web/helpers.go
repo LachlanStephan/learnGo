23762cedabf070e4733f41bd93be9bac97484900
LachlanStephan <ljstephan116@gmail.com>
2022-12-09 19:35:40 +1000

    added testing to see if templates work before returning them

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -29,10 +30,15 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
-	w.WriteHeader(status)
+	// we are going to check if the template is correct and safe to build
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
